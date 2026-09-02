2c90a5a193ecfc39c8fd530968cd1e51ab2ef953
Kuzin-Arseniy <kuzin.arsenij@icloud.com>
2026-09-02 12:37:44 +0300

    5.4 Введение в Playwright

---
+++ playwright_registration.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер Chromium (не в headless режиме, чтобы видеть действия)
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Переходим на страницу регистрации
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле email
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    expect(registration_email_input).to_be_visible()
+    registration_email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле username
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(username_input).to_be_visible()
+    username_input.fill("username")
+
+    # Заполняем поле password
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    expect(registration_password_input).to_be_visible()
+    registration_password_input.fill("password")
+
+    # Кликаем на кнопку "Registration"
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # Проверяем, что на странице "Dashboard" отображается заголовок "Dashboard"
+    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_header).to_be_visible()
