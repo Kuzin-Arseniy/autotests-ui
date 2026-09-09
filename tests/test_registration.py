3c04b1789bcaf86ca09579cff1907afe9c9e8f6d
Kuzin-Arseniy <kuzin.arsenij@icloud.com>
2026-09-09 11:01:27 +0300

    6.5 Фикстуры Pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,50 +1,33 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 import pytest
+
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration():
-    with sync_playwright() as playwright:
-        # Открываем браузер Chromium (не в headless режиме, чтобы видеть действия)
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()  # Создание контекста
-        page = context.new_page()  # Создание страницы
+def test_successful_registration(chromium_page: Page):
+    # Переходим на страницу регистрации
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        # Переходим на страницу регистрации
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    # Заполняем поле email
+    registration_email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    expect(registration_email_input).to_be_visible()
+    registration_email_input.fill("user.name@gmail.com")
 
-        # Заполняем поле email
-        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        expect(registration_email_input).to_be_visible()
-        registration_email_input.fill("user.name@gmail.com")
+    # Заполняем поле username
+    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(username_input).to_be_visible()
+    username_input.fill("username")
 
-        # Заполняем поле username
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        expect(username_input).to_be_visible()
-        username_input.fill("username")
+    # Заполняем поле password
+    registration_password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    expect(registration_password_input).to_be_visible()
+    registration_password_input.fill("password")
 
-        # Заполняем поле password
-        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        expect(registration_password_input).to_be_visible()
-        registration_password_input.fill("password")
+    # Кликаем на кнопку "Registration"
+    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-        # Кликаем на кнопку "Registration"
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        # Сохраняем состояние браузера
-        context.storage_state(path="browser-state.json")
-
-        # Проверяем, что на странице "Dashboard" отображается заголовок "Dashboard"
-        dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
-        expect(dashboard_title).to_have_text("Dashboard")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-
-        page.wait_for_timeout(3000)
+    # Проверяем, что на странице "Dashboard" отображается заголовок "Dashboard"
+    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
